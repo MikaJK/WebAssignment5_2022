b39fa657323ec20f3b4e572741a61fab0001c866
MikaJK <51448241+MikaJK@users.noreply.github.com>
2022-10-01 16:42:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -93,9 +93,13 @@ const getKuntaValue = (kuntaID) => {
 function testFileFunction() {
   let url =
     "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
-  fetchData(url).then((res) => {
-    showMapD(res);
-  });
+  fetchData(url)
+    .then((res) => {
+      showMapD(res);
+    })
+    .catch((err) => {
+      return Promise.reject();
+    });
 }
 
 async function startFunction() {
@@ -112,7 +116,9 @@ async function startFunction() {
       comingArray = data[1];
       testFileFunction();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      return Promise.reject();
+    });
 }
 
 const getFeature = (feature, layer) => {
